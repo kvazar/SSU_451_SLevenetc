0bcbd003aac7fe522553b8165605a8a30a7eb5a1
kvazar <onlworkru@gmail.com>
2016-04-26 23:27:59 +0300

    кек

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+//import java.util.Scanner;
+/**
+ *
+ * @author верыкенку
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         
+    
+  Object a = new Integer[10];
+Object[] b = new Integer[10];
+}
+}
